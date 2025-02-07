654751a17539b9ba54f69c5c0efb3e4b35f1d1a0
Benson <nyota.benson752@gmail.com>
2025-02-07 09:13:29 +0300

    updates

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-# app.py
 import streamlit as st
-from components.ga4_component import ga4
+import streamlit.components.v1 as components
 
-GA_ID = "G-SGLY9K9D0H"  # Replace with your Measurement ID
-
-ga4(GA_ID)  # Place this near the top of your app
-
-st.title("My Streamlit App with GA4")
-st.write("Welcome!")
-
-# ... rest of your Streamlit app
+# Embed the external HTML file using an iframe
+components.html('<iframe src="https://nyotabenson.github.io/googleanalytics/" width="100%" height="600"></iframe>', height=600)
